2f24df2305c1a5ab47aedfbb8b1e30f5913526c0
AnticipatedLearningMachine <51503150+AnticipatedLearningMachine@users.noreply.github.com>
2020-01-02 10:22:12 +0800

    Update main_Lorentz_x.py

--- code/main_Lorentz_x.py
+++ code/main_Lorentz_x.py
@@ -81,6 +81,8 @@ class NN(nn.Module):
         return nn.Sequential(*layers)
 
 units = [INPUT_SIZE]+[500, 200, 150, 50]
+net = NN(units)
+print(net)
 
 # plot function
 import matplotlib.pyplot as plt
