307fefe8a2cb1a9c9080313d2027c95c353b321a
AnticipatedLearningMachine <51503150+AnticipatedLearningMachine@users.noreply.github.com>
2020-02-05 16:00:37 +0800

    Update main_traffic_label2.py

--- code/main_traffic_label2.py
+++ code/main_traffic_label2.py
@@ -12,11 +12,12 @@ torch.backends.cudnn.benchmark = False
 torch.cuda.manual_seed(SEED)
 
 #initial parameters
-# LR1,LR2 --- 0.05
-# D --- 0.9
-# DROPOUT --- 0
-# EPOCH --- 150
-# alpha --- 0.1
+'''
+		LR1	    LR2		D		DROPOUT	period	EPOCH	alpha	Activate
+1-5:	0.05	0.05	0.9		0		0		150		0.1		Tanh
+6-10:	0.05	0.05	0.9		0		5		150		0.1		Tanh
+
+'''
 LR1 = 0.05
 LR2 = 0.05
 D = 0.9
