4ae0159e1f5b2b023077a97427b54a5ebe32855d
AnticipatedLearningMachine <51503150+AnticipatedLearningMachine@users.noreply.github.com>
2020-02-07 13:02:50 +0800

    Update main_Lorentz_nx.py

--- code/main_Lorentz_nx.py
+++ code/main_Lorentz_nx.py
@@ -305,7 +305,7 @@ consistent-training process
 fit(5,10)
 
 '''
-train the model by the consistent-training scheme. 
+after predicting all the 25 time points, train the model by the consistent-training scheme. 
 '''
 
 from tqdm import tqdm_notebook
@@ -323,7 +323,7 @@ def show_multi_curve_2(ys, title, legends, xxlabel, yylabel, start_point = 0, if
     plt.legend()
     plt.show()
 # steps of prediction
-step = 5
+step = 25
 repeat = 10
 
 # NEWINPUTS[i] ---- training inputs of φi 
