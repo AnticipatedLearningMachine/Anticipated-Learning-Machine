91a73276f51ac8296e2e1b7dce259d76afd4683d
AnticipatedLearningMachine <51503150+AnticipatedLearningMachine@users.noreply.github.com>
2020-02-07 14:52:06 +0800

    Update main_plankton.py

--- code/main_plankton.py
+++ code/main_plankton.py
@@ -287,7 +287,7 @@ fit(5,10)
 '''
 after predicting all the 30 time points, train the model by the consistent-training scheme. 
 '''
-# load the true value of oxygen (30 time points)
+# load the true value of oxygen (80 time points)
 Label = np.loadtxt('oxygen.txt')
 
 from tqdm import tqdm_notebook
