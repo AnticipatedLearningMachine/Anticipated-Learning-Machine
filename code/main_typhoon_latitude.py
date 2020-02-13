e3a627745262762550ec27506fd2c5127f9d5cc6
AnticipatedLearningMachine <51503150+AnticipatedLearningMachine@users.noreply.github.com>
2020-02-13 14:59:38 +0800

    Update main_typhoon_latitude.py

--- code/main_typhoon_latitude.py
+++ code/main_typhoon_latitude.py
@@ -305,6 +305,7 @@ fit(5,10)
 '''
 after predicting all the 27 time points, train the model by the consistent-training scheme. 
 '''
+'''
 # load the true value of latitude (77 time points)
 Label = np.loadtxt('latitude.txt')
 
@@ -457,3 +458,4 @@ print('test MAE', MAE(final_predicts, targets))
 print('test RMSE', RMSE(final_predicts, targets))
 print('test pearsonr', pearsonr(final_predicts, targets))
 print('test spearmanr',spearmanr(final_predicts,targets))
+'''
