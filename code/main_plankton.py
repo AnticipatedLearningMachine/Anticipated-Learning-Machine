63079f1de9d6b12065a93761dc2e5532ce80c701
AnticipatedLearningMachine <51503150+AnticipatedLearningMachine@users.noreply.github.com>
2020-02-13 14:56:03 +0800

    Update main_plankton.py

--- code/main_plankton.py
+++ code/main_plankton.py
@@ -281,6 +281,7 @@ fit(5,10)
 '''
 after predicting all the 30 time points, train the model by the consistent-training scheme. 
 '''
+'''
 # load the true value of oxygen (80 time points)
 Label = np.loadtxt('oxygen.txt')
 
@@ -433,3 +434,4 @@ print('test MAE', MAE(final_predicts, targets))
 print('test RMSE', RMSE(final_predicts, targets))
 print('test pearsonr', pearsonr(final_predicts, targets))
 print('test spearmanr',spearmanr(final_predicts,targets))
+'''
