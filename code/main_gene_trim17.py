057d21340c1fd9068629b637f4000afcead83acc
AnticipatedLearningMachine <51503150+AnticipatedLearningMachine@users.noreply.github.com>
2020-02-29 11:41:17 +0800

    Update main_gene_trim17.py

--- code/main_gene_trim17.py
+++ code/main_gene_trim17.py
@@ -274,7 +274,7 @@ the pairwise-training ends, if the result predicted here is good enough, there i
 following codes; else you can use the models saved by the above codes to initial input of the following
 consistent-training process
 '''
-fit(5,10)
+fit(6,10)
 
 '''
 because the data set of Gene is too short, we didn't train the model by the consistent-training scheme. 
